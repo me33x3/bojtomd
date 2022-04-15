dead111e8e64fcd2a20106b28500b01325edfe08
me33x3 <me33x3@gmail.com>
2022-04-16 00:48:41 +0900

    문제, 입력, 출력, 예제, 알고리즘 분류

--- bojtomd.py
+++ bojtomd.py
@@ -3,6 +3,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+import util
 from config import tier
 
 def crawl_baekjoon(problem_id):
@@ -20,7 +21,6 @@ def fetch_solvedac(problem_id):
 def write(data):
     data = json.loads(data)
     soup = crawl_baekjoon(data['problemId'])
-    print(data)
 
     base = '.\\files'
     path = base + '\\%s.md' % data['problemId']
@@ -39,13 +39,58 @@ def write(data):
 
     # 문제 정보
     problem_info = soup.select('#problem-info > tbody > tr > td')
-    print(problem_info)
 
     file.write('<center>\n\n')
     file.write('|시간 제한|메모리 제한|\n')
     file.write('|:---:|:---:|\n')
     file.write('|%s|%s|\n' % (problem_info[0].text, problem_info[1].text))
     file.write('</center>\n\n')
+
+    # 문제
+    problem_description = soup.select_one("#problem_description")
+
+    file.write('### 문제\n\n')
+    file.write('***\n\n')
+    file.write(util.get_content(problem_description))
+
+    # 입력
+    problem_input = soup.select_one("#problem_input")
+
+    file.write('### 입력\n\n')
+    file.write('***\n\n')
+    file.write(util.get_content(problem_input))
+
+    # 출력
+    problem_output = soup.select_one("#problem_output")
+
+    file.write('### 출력\n\n')
+    file.write('***\n\n')
+    file.write(util.get_content(problem_output))
+
+    # 제한
+    problem_limit = soup.select_one("#problem_limit")
+
+    # 예제
+    problem_sample = soup.select(".sampledata")
+
+    file.write('### 예제\n\n')
+    file.write('***\n\n')
+    file.write('|입력|출력|\n')
+    file.write('|:---|:---|\n')
+
+    for i in range(0, len(problem_sample), 2):
+        file.write('|%s|%s|\n' % (problem_sample[i].text.rstrip().replace('\r\n', '<br/>').replace('\n', '<br/>'), problem_sample[i+1].text.rstrip().replace('\r\n', '<br/>').replace('\n', '<br/>')))
+    file.write('\n')
+
+    # 힌트
+
+    # 알고리즘 분류
+    print(data['tags'])
+
+    file.write('### 알고리즘 분류\n\n')
+    file.write('***\n\n')
+    for i in range(len(data['tags'])):
+        file.write('* %s\n' % data['tags'][i]['displayNames'][1]['name'])
 
     file.close()
 
